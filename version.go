fb02aebfe206317431e725ea45d4a384f8ee9d40
GitHub Action <action@github.com>
2025-03-09 00:07:53 +0000

    Bump version to 0.1.4

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ import (
 
 // Version information - this will be updated automatically by GitHub Actions
 const (
-	VERSION = "0.1.3"
+	VERSION = "0.1.4"
 )
 
 // VersionInfo holds extended version information
